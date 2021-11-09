3e197680d61714ba8f2a6cfab77594668be57939
Dimuthu <dimuthu.s@sits.lk>
2021-11-09 04:29:24 +0100

    Front end code commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class Home extends Component {
+    render() {
+        return (
+            <div>
+    <div><br></br><br></br>
+               <div class="row">
+<div class="jumbotron col-lg-4 offset-lg-4">
+     <h3 class="text-center">HOME</h3>
+  
+     
+
+</div>
+
+               </div>
+          </div>            </div>
+        )
+    }
+}
+
+export default Home
